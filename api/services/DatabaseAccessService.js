0c0fd966a55821a347987fc194421f0762bc35c6
Marcellin Nshimiyimana <mn2587@columbia.edu>
2016-12-20 15:22:17 -0500

    Update DatabaseAccessService.js

--- api/services/DatabaseAccessService.js
+++ api/services/DatabaseAccessService.js
@@ -5,8 +5,9 @@
 * @Last modified time: 2016-12-05T19:14:40-05:00
 */
 'use strict';
+let DailyJokeAccess = require('./DatabaseAccessPrivateDailyJoke.js').main;
 let UserAccess = require('./DatabaseAccessPrivateUser.js').main;
 
 // api/services/DatabaseAccessService.js
-const service = Object.assign({}, UserAccess);
+const service = Object.assign({}, DailyJokeAccess, UserAccess);
 module.exports = service;
